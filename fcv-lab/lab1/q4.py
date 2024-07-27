424649083f06d28f3c6c0c94f98193df347cd1e1
balabcode <srinivasankumar@proton.me>
2024-07-27 15:35:56 +0530

    Add files via upload

--- fcv-lab/lab1/q4.py
+++ fcv-lab/lab1/q4.py
@@ -1,10 +1,9 @@
 import cv2
-
+import numpy as np
 # Write a simple program to draw rectangle.
 
-img = cv2.imread('q1_img.png')
-
-img = cv2.rectangle(img,(0,0),(800,800),(0,0,255),3)
+img = np.zeros((512,512,3), np.uint8)
+img = cv2.rectangle(img,(10,10),(502, 502),(0,0,255),3)
 
 cv2.imshow('img',img)
 
